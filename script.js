f0a9d7f20ef3e410954c3682ff0956175e00d8c6
Adillex <76777372+adillex@users.noreply.github.com>
2025-02-15 00:19:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -10,36 +10,42 @@ function changeText() {
     text.textContent = "created by adi11ex";
     text.classList.add('show');
     changeText();
-  }, 16500);
+  }, 22500);
   setTimeout(() => {
     text.textContent = "created by adi11ex";
+  }, 20000);
+    setTimeout(() => {
+    text.textContent = "какой кринж, хорошо что я пьян";
+  }, 17500);
+    setTimeout(() => {
+    text.textContent = "с 14 февраля";
   }, 15000);
   setTimeout(() => {
-    text.textContent = "с 14 февраля, за*бал";
+    text.textContent = "и из всех кого я хотел бы знать";
   }, 12000);
   setTimeout(() => {
-    text.textContent = "которых я знаю";
+    text.textContent = "из всех кого я знаю";
   }, 10500);
   setTimeout(() => {
-    text.textContent = "альтушек";
+    text.textContent = "заботливая";
   }, 9000);
   setTimeout(() => {
-    text.textContent = "из всех";
+    text.textContent = "обоятельная";
   }, 7500);
   setTimeout(() => {
     text.textContent = "угарная";
   }, 6000);
   setTimeout(() => {
-    text.textContent = "нефорская";
+    text.textContent = "приятная";
   }, 4500);
   setTimeout(() => {
     text.textContent = "милая";
   }, 3000);
   setTimeout(() => {
-    text.textContent = "ты самый";
+    text.textContent = "ты самая";
   }, 1500);
   setTimeout(() => {
-    text.textContent = "Алуа";
+    text.textContent = "В этот прекраснный день хочу сказать";
   }, 0);
   
 }
